42c95ee7ca4d299864678abf42d7d96b15727e8c
Vinícius Brandão <viniciusbrandao9417@gmail.com>
2026-06-09 18:57:42 -0300

    feat: utilizando variaveis e funcoes no JSX.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 
 function App() {
+
+function sum(a, b) {
+  return a + b 
+}
+const nome = "Vinícius"
+const idade = 27
   return (
     <div className="App">
       <h1>Olá react!</h1>
-      <p>Meu primeiro app</p>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {nome}, você tem {idade} anos e a soma dos números é {sum(1,5)} anos</p>
     </div>
   );
 }
